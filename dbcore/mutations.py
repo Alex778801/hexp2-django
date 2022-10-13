d7ebd0f91780778d2b2a2fe598e3e15e4c3f32c3
Alexey Sorokin <sorokin80@gmail.com>
2022-10-13 16:08:34 +0300

    jwt auth

---
+++ dbcore/mutations.py
@@ -0,0 +1,24 @@
+import graphene
+import graphql_jwt
+from .types import UserType
+
+
+class ObtainJSONWebToken(graphql_jwt.JSONWebTokenMutation):
+    user = graphene.Field(UserType)
+
+    @classmethod
+    def resolve(cls, root, info, **kwargs):
+        return cls(user=info.context.user)
+
+
+class Mutation(graphene.ObjectType):
+    # token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+    token_auth = ObtainJSONWebToken.Field()
+    verify_token = graphql_jwt.Verify.Field()
+    refresh_token = graphql_jwt.Refresh.Field()
+
+
+
+
+
+
