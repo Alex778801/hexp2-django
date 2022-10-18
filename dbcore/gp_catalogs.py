b6dc2867741290e8849107922ccd8eca887778ae
Alexey Sorokin <sorokin80@gmail.com>
2022-10-18 23:54:49 +0300

    cats all

--- dbcore/gp_catalogs.py
+++ dbcore/gp_catalogs.py
@@ -5,6 +5,7 @@ from graphql_jwt.decorators import login_required
 
 
 from dbcore.models import Project, Agent, CostType
+from dbcore.models_base import HierarchyOrderModelExt
 from ua.models import logUserAction
 
 
@@ -12,7 +13,7 @@ from ua.models import logUserAction
 # - Т И П Ы
 # ----------------------------------------------------------------------------------------------------------------------
 
-
+# Базовый класс каталога
 class CustomCat:
     id = graphene.Int()
     pid = graphene.Int()
@@ -25,22 +26,40 @@ class CustomCat:
         else:
             return -1
 
-    def resolve_pid(self: Project, info):
+    def resolve_pid(self: HierarchyOrderModelExt, info):
         if self.parent_id is not None:
             return self.parent_id
         else:
             return -1
 
-    def resolve_grp(self: Project, info):
+    def resolve_grp(self: HierarchyOrderModelExt, info):
         return self.isGrp
 
-    def resolve_ord(self: Project, info):
+    def resolve_ord(self: HierarchyOrderModelExt, info):
         return self.order
 
 
+# Проекты
 class ProjectType(DjangoObjectType, CustomCat):
     class Meta:
         model = Project
+
+
+# Агенты
+class AgentType(DjangoObjectType, CustomCat):
+    class Meta:
+        model = Agent
+
+
+# Статьи
+class CostTypeType(DjangoObjectType, CustomCat):
+    class Meta:
+        model = CostType
+
+    out = graphene.Boolean()
+
+    def resolve_out(self: CostType, info):
+        return self.isOutcome
 
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -48,10 +67,20 @@ class ProjectType(DjangoObjectType, CustomCat):
 # ----------------------------------------------------------------------------------------------------------------------
 
 
-class ProjectsQuery(graphene.ObjectType):
+class CatalogsQuery(graphene.ObjectType):
+    # Проекты
     project = graphene.Field(ProjectType, id=graphene.Int())
     projects = graphene.List(ProjectType)
+    # Агенты
+    agent = graphene.Field(AgentType, id=graphene.Int())
+    agents = graphene.List(AgentType)
+    # Статьи
+    costtype = graphene.Field(CostTypeType, id=graphene.Int())
+    costtypes = graphene.List(CostTypeType)
 
+
+    # -------------
+    # Проект
     @login_required
     def resolve_project(self, info, **kwargs):
         id = kwargs.get('id')
@@ -59,9 +88,38 @@ class ProjectsQuery(graphene.ObjectType):
             return Project.objects.get(pk=id)
         return None
 
+    # ПроектЫ
     @login_required
     def resolve_projects(self, info, **kwargs):
         return Project.objects.all()
+
+    # -------------
+    # Агент
+    @login_required
+    def resolve_agent(self, info, **kwargs):
+        id = kwargs.get('id')
+        if id is not None:
+            return Agent.objects.get(pk=id)
+        return None
+
+    # АгентЫ
+    @login_required
+    def resolve_agents(self, info, **kwargs):
+        return Agent.objects.all()
+
+    # -------------
+    # Статья
+    @login_required
+    def resolve_costtype(self, info, **kwargs):
+        id = kwargs.get('id')
+        if id is not None:
+            return CostType.objects.get(pk=id)
+        return None
+
+    # Статьи
+    @login_required
+    def resolve_costtypes(self, info, **kwargs):
+        return CostType.objects.all()
 
 
 # ----------------------------------------------------------------------------------------------------------------------
