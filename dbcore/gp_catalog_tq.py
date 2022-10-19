577a3bb850b73671231951d06eb9b2af72b14449
Alexey Sorokin <sorokin80@gmail.com>
2022-10-19 19:47:22 +0300

    proj elem 1

---
+++ dbcore/gp_catalog_tq.py
@@ -0,0 +1,133 @@
+import json
+import graphene
+from graphene_django import DjangoObjectType
+from graphql_jwt.decorators import login_required
+
+
+from dbcore.models import Project, Agent, CostType
+from dbcore.models_base import HierarchyOrderModelExt
+from dj.myutils import CustomJSONEncoder
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# - Т И П Ы
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+# Базовый класс каталога
+class CustomCat:
+    id = graphene.Int()
+    pid = graphene.Int()
+    grp = graphene.Boolean()
+    ord = graphene.Int()
+
+    def resolve_id(self, info):
+        if self is not None:
+            return self.id
+        else:
+            return -1
+
+    def resolve_pid(self: HierarchyOrderModelExt, info):
+        if self.parent_id is not None:
+            return self.parent_id
+        else:
+            return -1
+
+    def resolve_grp(self: HierarchyOrderModelExt, info):
+        return self.isGrp
+
+    def resolve_ord(self: HierarchyOrderModelExt, info):
+        return self.order
+
+
+# Проекты
+class ProjectType(DjangoObjectType, CustomCat):
+    class Meta:
+        model = Project
+
+    path = graphene.String()
+    prefCostTypeGroupTree = graphene.String()
+
+    def resolve_path(self: Project, info):
+        return self.getParentsList()
+
+    def resolve_prefCostTypeGroupTree(self: Project, info):
+        tmp = CostType.getGroupsTree()
+
+        res = json.dumps(tmp, ensure_ascii=False, cls=CustomJSONEncoder).encode('utf8')
+        return res
+
+# Агенты
+class AgentType(DjangoObjectType, CustomCat):
+    class Meta:
+        model = Agent
+
+
+# Статьи
+class CostTypeType(DjangoObjectType, CustomCat):
+    class Meta:
+        model = CostType
+
+    out = graphene.Boolean()
+
+    def resolve_out(self: CostType, info):
+        return self.isOutcome
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# З А П Р О С Ы
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+class CatalogsQuery(graphene.ObjectType):
+    # Проекты
+    project = graphene.Field(ProjectType, id=graphene.Int())
+    projects = graphene.List(ProjectType)
+    # Агенты
+    agent = graphene.Field(AgentType, id=graphene.Int())
+    agents = graphene.List(AgentType)
+    # Статьи
+    costtype = graphene.Field(CostTypeType, id=graphene.Int())
+    costtypes = graphene.List(CostTypeType)
+
+    # -------------
+    # Проект
+    @login_required
+    def resolve_project(self, info, **kwargs):
+        id = kwargs.get('id')
+        if id is not None:
+            return Project.objects.get(pk=id)
+        return None
+
+    # ПроектЫ
+    @login_required
+    def resolve_projects(self, info, **kwargs):
+        return Project.objects.all()
+
+    # -------------
+    # Агент
+    @login_required
+    def resolve_agent(self, info, **kwargs):
+        id = kwargs.get('id')
+        if id is not None:
+            return Agent.objects.get(pk=id)
+        return None
+
+    # АгентЫ
+    @login_required
+    def resolve_agents(self, info, **kwargs):
+        return Agent.objects.all()
+
+    # -------------
+    # Статья
+    @login_required
+    def resolve_costtype(self, info, **kwargs):
+        id = kwargs.get('id')
+        if id is not None:
+            return CostType.objects.get(pk=id)
+        return None
+
+    # Статьи
+    @login_required
+    def resolve_costtypes(self, info, **kwargs):
+        return CostType.objects.all()
--- dbcore/gp_catalogs_mut.py
+++ dbcore/gp_catalogs_mut.py
@@ -2,14 +2,13 @@ import json
 import graphene
 from graphql_jwt.decorators import login_required
 
-from django.contrib.auth.models import User
+
 from dbcore.models import Project, Agent, CostType
-from dbcore.models_base import isAdmin, aclCanMod
-from ua.models import logUserAction, modelDiff
+from ua.models import logUserAction
 
 
 # ----------------------------------------------------------------------------------------------------------------------
-# К А Т А Л О Г И
+# М У Т А Ц И И
 # ----------------------------------------------------------------------------------------------------------------------
 
 
@@ -17,11 +16,11 @@ from ua.models import logUserAction, modelDiff
 def getItemModel(model):
     pModel = model.lower()
     itemModel = None
-    if pModel == 'project':
+    if pModel == 'projects':
         itemModel = Project
-    elif pModel == 'agent':
+    elif pModel == 'agents':
         itemModel = Agent
-    elif pModel == 'costtype':
+    elif pModel == 'costtypes':
         itemModel = CostType
     return itemModel
 
@@ -42,13 +41,12 @@ class CreateCatObject(graphene.Mutation):
         itemModel = getItemModel(model)
         newItem = itemModel()
         if pid != -1:
-            newItem.parent = itemModel.objects.get(pk=pid)
+            newItem.parent = Project.objects.get(pk=pid)
         newItem.isGrp = isGrp
         newItem.name = name
-        newItem.owner = info.context.user
         newItem.save()
-        logUserAction(info.context.user, itemModel, f"new {'group' if isGrp else 'element'} '{newItem.pk}:{newItem.name}'",
-                      link=f"/{model}/{newItem.pk}")
+        # TODO: link=makeLinkForModel(item)
+        logUserAction(info.context.user, itemModel, f"new {'group' if isGrp else 'element'} '{newItem.pk}:{newItem.name}'")
         # noinspection PyArgumentList
         return CreateCatObject(ok=True, result=f'Create in {itemModel}, id={newItem.pk}')
 
@@ -98,6 +96,7 @@ class DeleteCatObjects(graphene.Mutation):
             else:
                 delRefGroup = itemModel.objects.get(isGrp=True, name='_DELREF')
                 item.changeParent(delRefGroup)
+                # TODO: link=makeLinkForModel(item)
                 logUserAction(info.context.user, itemModel, f"move _DELREF, '{id}:{item.name}'")
         # noinspection PyArgumentList
         return DeleteCatObjects(ok=True, result=f'Delete in {itemModel}, ids={ids}')
@@ -120,10 +119,10 @@ class RenameCatObject(graphene.Mutation):
         oldName = item.name
         item.name = name
         item.save()
-        logUserAction(info.context.user, itemModel, f"rename '{id}:{name}'", diff=f"name: {oldName} -> {name}",
-                      link=f"/{model}/{item.pk}")
+        # TODO: link=makeLinkForModel(item)
+        logUserAction(info.context.user, itemModel, f"rename '{id}:{name}'", diff=f"name: {oldName} -> {name}")
         # noinspection PyArgumentList
-        return RenameCatObject(ok=True, result=f"Rename in {itemModel}, id={id}")
+        return CreateCatObject(ok=True, result=f"Rename in {itemModel}, id={id}")
 
 
 # Изменение порядка объекта справочника
@@ -142,8 +141,8 @@ class ChangeOrderCatObject(graphene.Mutation):
         item = itemModel.objects.get(pk=id)
         oldOrder = item.order
         item.changeOrder(order)
-        logUserAction(info.context.user, itemModel, f"change order '{item.pk}:{item.name}'", diff=f"order: {oldOrder} -> {order}",
-                      link=f"/{model}/{item.pk}")
+        # TODO: link=makeLinkForModel(item)
+        logUserAction(info.context.user, itemModel, f"change order '{item.pk}:{item.name}'", diff=f"order: {oldOrder} -> {order}")
         # noinspection PyArgumentList
         return ChangeOrderCatObject(ok=True, result=f"Change order in {itemModel}, id={id}")
 
@@ -170,8 +169,8 @@ class ChangeParentCatObjects(graphene.Mutation):
             item = itemModel.objects.get(pk=itemId)
             oldPid = item.parent.pk if item.parent is not None else -1
             item.changeParent(newParent)
-            logUserAction(info.context.user, itemModel, f"change parent '{item.pk}:{item.name}'", diff=f"pid: {oldPid} -> {pid}",
-                          link=f"/{model}/{item.pk}")
+            # TODO: link=makeLinkForModel(item)
+            logUserAction(info.context.user, itemModel, f"change parent '{item.pk}:{item.name}'", diff=f"pid: {oldPid} -> {pid}")
         # noinspection PyArgumentList
         return ChangeParentCatObjects(ok=True, result=f"Change parent in {itemModel}, ids={ids}")
 
@@ -199,128 +198,7 @@ class CopyCatObjects(graphene.Mutation):
             clonedItem = item.clone(parent)
             clonedItem.owner = info.context.user
             clonedItem.save()
-            logUserAction(info.context.user, itemModel, f"clone to '{clonedItem.pk}:{clonedItem.name}' from '{item.pk}:{item.name}'",
-                          link=f"/{model}/{clonedItem.pk}")
+            # TODO: link=makeLinkForModel(item)
+            logUserAction(info.context.user, itemModel, f"clone to '{clonedItem.pk}:{clonedItem.name}' from '{item.pk}:{item.name}'")
         # noinspection PyArgumentList
         return CopyCatObjects(ok=True, result=f"Clone in {itemModel}, ids={ids}")
-
-
-# ----------------------------------------------------------------------------------------------------------------------
-# Обновление элемента справочника
-
-# Обновить - ПРОЕКТЫ
-class UpdateProject(graphene.Mutation):
-    class Arguments:
-        id = graphene.Int()
-        name = graphene.String()
-        prefCostTypeGroup = graphene.Int()
-        prefAgentGroup = graphene.Int()
-        prefFinOperLogIntv = graphene.Int()
-        prefFinOperLogIntvN = graphene.Int()
-        owner = graphene.String()
-        acl = graphene.String()
-
-    ok = graphene.Boolean()
-    result = graphene.String()
-
-    @login_required
-    def mutate(root, info, id, name, owner, acl,
-               prefCostTypeGroup=None, prefAgentGroup=None, prefFinOperLogIntv=None, prefFinOperLogIntvN=1):
-        project = Project.objects.get(pk=id)
-        # Изменять может только админ или владелец!
-        if not isAdmin(info.context.user) and not info.context.user == project.owner and project.owner is not None:
-            raise Exception("У вас нет прав на изменение данного объекта!")
-        # --
-        project.name = name
-        project.prefCostTypeGroup = CostType.objects.filter(pk=prefCostTypeGroup).first()
-        project.prefAgentGroup = Agent.objects.filter(pk=prefAgentGroup).first()
-        project.prefFinOperLogIntv = prefFinOperLogIntv
-        project.prefFinOperLogIntv_n = prefFinOperLogIntvN
-        project.acl = acl
-        project.owner = User.objects.filter(username=owner).first()
-        # --
-        diff = modelDiff(Project.objects.get(pk=project.pk), project)
-        project.save()
-        logUserAction(info.context.user, Project, f"save '{project.pk}:{project.name}'", link=f"/project/{project.pk}", diff=diff)
-        # noinspection PyArgumentList
-        return UpdateProject(ok=True, result=f"Update in {Project}, id={project.pk}")
-
-# Обновить Заметки проекта
-class UpdateProjectInfo(graphene.Mutation):
-    class Arguments:
-        id = graphene.Int()
-        projinfo = graphene.String()
-
-    ok = graphene.Boolean()
-    result = graphene.String()
-
-    @login_required
-    def mutate(root, info, id, projinfo):
-        project = Project.objects.get(pk=id)
-        # -- Безопасность ACL
-        canMod = aclCanMod(project, project.acl, info.context.user)[0]
-        if not canMod:
-            raise Exception("У вас нет прав на изменение данного объекта!")
-        # --
-        project.info = projinfo
-        project.save()
-        logUserAction(info.context.user, Project, f"modify project info '{project.pk}:{project.name}'", link=f"/project/{project.pk}")
-        # noinspection PyArgumentList
-        return UpdateProject(ok=True, result=f"Modify project info in {Project}, id={project.pk}")
-
-# Обновить - СТАТЬЯ
-class UpdateCostType(graphene.Mutation):
-    class Arguments:
-        id = graphene.Int()
-        name = graphene.String()
-        color = graphene.String()
-        isOutcome = graphene.Boolean()
-        owner = graphene.String()
-
-    ok = graphene.Boolean()
-    result = graphene.String()
-
-    @login_required
-    def mutate(root, info, id, name, color, isOutcome, owner):
-        costType = CostType.objects.get(pk=id)
-        # Изменять может только админ или владелец!
-        if not isAdmin(info.context.user) and not info.context.user == costType.owner and costType.owner is not None:
-            raise Exception("У вас нет прав на изменение данного объекта!")
-        # --
-        costType.name = name
-        costType.color = color
-        costType.isOutcome = isOutcome
-        costType.owner = User.objects.filter(username=owner).first()
-        # --
-        diff = modelDiff(CostType.objects.get(pk=costType.pk), costType)
-        costType.save()
-        logUserAction(info.context.user, CostType, f"save '{costType.pk}:{costType.name}'", link=f"/costtype/{costType.pk}", diff=diff)
-        # noinspection PyArgumentList
-        return UpdateCostType(ok=True, result=f"Update in {CostType}, id={costType.pk}")
-
-
-# Обновить - АГЕНТ
-class UpdateAgent(graphene.Mutation):
-    class Arguments:
-        id = graphene.Int()
-        name = graphene.String()
-        owner = graphene.String()
-
-    ok = graphene.Boolean()
-    result = graphene.String()
-
-    @login_required
-    def mutate(root, info, id, name, owner):
-        agent = Agent.objects.get(pk=id)
-        # Изменять может только админ или владелец!
-        if not isAdmin(info.context.user) and not info.context.user == agent.owner and agent.owner is not None:
-            raise Exception("У вас нет прав на изменение данного объекта!")
-        # --
-        agent.name = name
-        agent.owner = User.objects.filter(username=owner).first()
-        # --
-        diff = modelDiff(Agent.objects.get(pk=agent.pk), agent)
-        agent.save()
-        logUserAction(info.context.user, Agent, f"save '{agent.pk}:{agent.name}'", link=f"/agent/{agent.pk}", diff=diff)
-        # noinspection PyArgumentList
-        return UpdateAgent(ok=True, result=f"Update in {Agent}, id={agent.pk}")
