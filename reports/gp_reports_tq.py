29e3b34a0253b3878033e10f87ba9360d1d12bdb
Alexey Sorokin <sorokin80@gmail.com>
2022-11-01 19:15:37 +0300

    report2 2

--- reports/gp_reports_tq.py
+++ reports/gp_reports_tq.py
@@ -7,6 +7,7 @@ from graphql_jwt.decorators import login_required
 from dbcore.models import Project, Agent, CostType, FinOper
 from dbcore.models_base import HierarchyOrderModelExt, aclGetUsersList, isAdmin, aclCanRead, aclCanReport
 from reports.report001 import report001
+from reports.report002 import report002
 
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -26,6 +27,13 @@ from reports.report001 import report001
 
 class ReportsQuery(graphene.ObjectType):
     report001 = graphene.String(projectId=graphene.Int())
+    report002 = graphene.String(projectId=graphene.Int(),
+                                beginDate=graphene.Int(),
+                                endDate=graphene.Int(),
+                                costTypeId=graphene.Int(),
+                                agentFromId=graphene.Int(),
+                                agentToId=graphene.Int()
+                                )
 
     # Отчет 001
     @login_required
@@ -37,5 +45,18 @@ class ReportsQuery(graphene.ObjectType):
             raise Exception("У вас нет прав на просмотр данного объекта!")
         # --
         tmp = report001(projectId)
+        res = json.dumps(tmp, ensure_ascii=True, sort_keys=True, default=str)
+        return res
+
+    # Отчет 002
+    @login_required
+    def resolve_report002(self, info, projectId, beginDate=-1, endDate=-1, costTypeId=-1, agentFromId=-1, agentToId=-1):
+        project = Project.objects.get(pk=projectId)
+        # -- Безопасность ACL
+        canReport = aclCanReport(project, project.acl, info.context.user)[0]
+        if not canReport:
+            raise Exception("У вас нет прав на просмотр данного объекта!")
+        # --
+        tmp = report002(projectId, beginDate, endDate, costTypeId, agentFromId, agentToId)
         res = json.dumps(tmp, ensure_ascii=True, sort_keys=True, default=str)
         return res
