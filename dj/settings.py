c58308ef12eeb983c6b4873b4f800fb96168a875
Alexey Sorokin <sorokin80@gmail.com>
2022-11-08 08:59:02 +0300

    divide code and data for multy sites 1

--- dj/settings.py
+++ dj/settings.py
@@ -9,11 +9,13 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+import sys
 from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR_UP = Path(__file__).resolve().parent.parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -134,7 +136,9 @@ STATIC_ROOT = BASE_DIR / "static/"
 
 # MEDIA
 
-MEDIA_ROOT = BASE_DIR / 'media/'
+#MEDIA_ROOT = BASE_DIR / 'media/'
+MEDIA_ROOT = BASE_DIR_UP / 'media/'
+
 MEDIA_URL = '/media/'
 
 # Default primary key field type
@@ -196,7 +200,10 @@ PROJECT_INFO_DIR = 'projectinfo_media'
 # ----------------------------------------------------------------------------------------------------------------------
 # ----------------------------------------------------------------------------------------------------------------------
 
+sys.path.append(str(BASE_DIR_UP))
+
 try:
-    from .settings_prod import *
+    from settings_prod import *
 except ImportError as e:
+    print('Import settings_prod error!')
     pass
